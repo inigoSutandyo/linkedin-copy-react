bb52d1a33e2f76c9fc2c1daa7472c85254349f85
Inigo Hersanta Sutandyo <86969846+inigoSutandyo@users.noreply.github.com>
2022-10-17 22:59:40 +0700

    small changes

--- src/components/message/OtherMessage.tsx
+++ src/components/message/OtherMessage.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { ApiURL } from '../../utils/Server'
 import SharedPost from '../post/SharedPost'
+import UserComponent from '../user/UserComponent'
 
 type Props = {
     message: Message
 }
 
 const OtherMessage = (props: Props) => {
+  const [user, setUser] = useState<User>()
+  useEffect(() => {
+    if (props.message.profileid < 1 ) return
+
+    axios.get(ApiURL("/user/otherprofile"), {
+      params: {
+          id: props.message.profileid
+      }
+    })
+    .then((response) => {
+      setUser(response.data.user)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [props])
   return (
     <div className="mx-2 other-message">
         {props.message.post  ? (
@@ -19,6 +38,10 @@ const OtherMessage = (props: Props) => {
                 maxWidth: "240px"
               }}/>
             </div>
+          </>
+        ) : props.message.profileid >= 1 && user ? (
+          <>
+            <UserComponent user={user}/>
           </>
         ) : (
           <>
